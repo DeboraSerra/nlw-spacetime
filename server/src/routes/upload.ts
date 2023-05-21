57a4c2f7e52370c9192e940edf1d066163ece737
Debora Serra <debora.r.serra@gmail.com>
2023-05-21 15:35:33 -0300

    feat: update backend to upload and serve files

---
+++ server/src/routes/upload.ts
@@ -0,0 +1,45 @@
+import { randomUUID } from 'crypto'
+import { FastifyInstance } from 'fastify'
+import { createWriteStream } from 'fs'
+import { extname, resolve } from 'path'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+
+const pump = promisify(pipeline)
+
+export async function uploadRoutes(app: FastifyInstance) {
+  app.post('/upload', async (req, res) => {
+    const data = await req.file({
+      limits: {
+        fileSize: 5_242_880, // 5mb
+      },
+    })
+
+    if (!data) {
+      return res.status(400).send()
+    }
+
+    const mimetypeRegex = /^(image|video)\/[a-zA-Z]+/
+    const isValidFormat = mimetypeRegex.test(data.mimetype)
+
+    if (isValidFormat) {
+      return res.status(400).send()
+    }
+
+    const fileId = randomUUID()
+    const extension = extname(data.filename)
+    const fileName = fileId.concat(extension)
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads/', fileName),
+    )
+
+    await pump(data.file, writeStream)
+
+    const fullUrl = req.protocol.concat('://').concat(req.hostname)
+
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+
+    return { fileUrl }
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,19 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
 
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 app.register(cors, {
   origin: true,
   // origin: ['http://localhost:3000'],
@@ -14,6 +22,7 @@ app.register(cors, {
 app.register(jwt, { secret: 'spacetime' })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
