b08f340e0b4b51f97d0c9f85e74e67c1abfc0f41
Debora Serra <debora.r.serra@gmail.com>
2023-05-21 09:05:31 -0300

    feat: add github oauth link

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Blur from '@/components/Blur'
 import Copyright from '@/components/Copyright'
+import EmptyMemories from '@/components/EmptyMemories'
 import Hero from '@/components/Hero'
 import SignIn from '@/components/SignIn'
 import Stripes from '@/components/Stripes'
@@ -7,7 +8,6 @@ import Stripes from '@/components/Stripes'
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
-      {/** Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <Blur />
         <Stripes />
@@ -15,17 +15,8 @@ export default function Home() {
         <Hero />
         <Copyright />
       </div>
-      {/** Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Voce ainda nao registrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
