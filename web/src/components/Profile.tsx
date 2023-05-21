e6269cfbcac2dd3ad48999c77ca84cbecf558cb8
Debora Serra <debora.r.serra@gmail.com>
2023-05-21 10:26:36 -0300

    fix: fix profile width

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -12,7 +12,7 @@ const Profile = () => {
         height={40}
         className="h-10 w-10 rounded-full"
       />
-      <p className="max-w-[140px] text-sm leading-snug">
+      <p className="max-w-[280px] text-sm leading-snug">
         {name}
         <a
           href=""
